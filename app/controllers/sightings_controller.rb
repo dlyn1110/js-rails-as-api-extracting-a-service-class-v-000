38f0ff871ebde905d874ea0ea4cdd11c1d80296f
Dani Lyn <dlyn1110@gmail.com>
2020-05-26 20:16:09 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,7 +1,7 @@
 class SightingsController < ApplicationController
 
   def index
-    @sightings = Sighting.all
+    sightings = Sighting.all
     render json: SightingSerializer.new(sightings).to_serialized_json
   end
 
